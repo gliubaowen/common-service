06bd5602dd4fa6060529b8aad5b2e331bab90d96
BaoWenLiu <BaoWenLiu@9.200.60.80>
2019-09-09 21:21:11 +0800

    配置代码componentScan && sftp 测试

--- src/main/java/com/ibm/common/service/CommonServiceApplication.java
+++ src/main/java/com/ibm/common/service/CommonServiceApplication.java
@@ -2,8 +2,10 @@ package com.ibm.common.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ibm.common"})
 public class CommonServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/ibm/common/service/junit/JunitTest.java
@@ -0,0 +1,17 @@
+package com.ibm.common.service.junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JunitTest {
+
+	@Test
+	public void Test() {
+		System.out.println("Hello Spring Boot");
+	}
+
+}
