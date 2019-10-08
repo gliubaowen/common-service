24b4777c8715c7035955b71824ed5b6cb90856d0
BaoWenLiu <BaoWenLiu@DESKTOP-CIITADI>
2019-10-08 23:53:07 +0800

    修改 配置文件为yaml && 添加spring-shell支持

--- src/main/java/com/ibm/common/service/CommonServiceApplication.java
+++ src/main/java/com/ibm/common/service/CommonServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ibm.common"})
+@ComponentScan(basePackages = { "com.ibm.common" })
 public class CommonServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ibm/common/service/shell/command/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * shell 自定义命令
+ * 
+ * @author LiuBaoWen
+ *
+ */
+package com.ibm.common.service.shell.command;
