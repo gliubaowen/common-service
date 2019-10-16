29eb017309578f36262b42e5eb2803af89d66c49
BaoWenLiu <BaoWenLiu@DESKTOP-CIITADI>
2019-10-16 11:05:23 +0800

    实现 shellCommand 调用Controller 案例

--- src/main/java/com/ibm/common/service/controller/CommonServiceController.java
+++ src/main/java/com/ibm/common/service/controller/CommonServiceController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
-//@RequestMapping("service")
+@RequestMapping("service")
 public class CommonServiceController {
 
 	@RequestMapping("/")
---
+++ src/main/java/com/ibm/common/service/shell/command/CommonServiceControllerCommands.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.ibm.common.service.shell.command;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+import com.ibm.common.service.controller.CommonServiceController;
+
+/**
+ * @author LiuBaoWen
+ *
+ */
+@ShellComponent
+public class CommonServiceControllerCommands {
+
+	@Autowired
+	private CommonServiceController commonServiceController;
+
+	/**
+	 * 
+	 * 
+	 * @author LiuBaoWen
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	@ShellMethod(value = "Controller helloWorld.", key = "ctlsum")
+	public String add() {
+		return commonServiceController.helloWorld();
+	}
+	
+}
