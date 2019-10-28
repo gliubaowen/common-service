cbd9229e2f40d80b5880cd176ca1044002f1bc2d
BaoWenLiu <BaoWenLiu@DESKTOP-CIITADI>
2019-10-28 20:35:03 +0800

    集成配置优化rocketMQ

---
+++ src/main/java/com/ibm/common/service/rocketmq/consumer/RocketMQConsumerConfig.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.ibm.common.service.rocketmq.consumer;
+
+import java.util.List;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ibm.common.service.CommonServiceApplication;
+
+/**
+ * RocketMQ Consumer
+ * 
+ * @author LiuBaoWen
+ *
+ */
+@Configuration
+public class RocketMQConsumerConfig {
+
+	private static final Logger log = LoggerFactory.getLogger(CommonServiceApplication.class);
+
+	@Value("${mq.rocketmq.producer-group}")
+	private String producerGroup;
+	
+	@Value("${mq.rocketmq.namesrv-addr}")
+	private String namesrvAddr;
+	
+	@Bean
+	public void consumer() throws Exception {
+		// Instantiate with specified consumer group name.
+		DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(producerGroup);
+
+		// Specify name server addresses.
+		consumer.setNamesrvAddr(namesrvAddr);
+
+		// Subscribe one more more topics to consume.
+		consumer.subscribe("TopicTest", "*");
+		// Register callback to execute on arrival of messages fetched from brokers.
+		consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+			@Override
+			public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+				log.info("{} Receive New Messages: {}", Thread.currentThread().getName(), msgs);
+				return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+			}
+		});
+
+		// Launch the consumer instance.
+		consumer.start();
+
+		log.info("RocketMQ Consumer Started.");
+	}
+
+}
---
+++ src/main/java/com/ibm/common/service/shell/command/RocketMQProducerCommands.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package com.ibm.common.service.shell.command;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+import com.ibm.common.service.CommonServiceApplication;
+
+/**
+ * @author LiuBaoWen
+ *
+ */
+@ShellComponent
+public class RocketMQProducerCommands {
+
+	private static final Logger log = LoggerFactory.getLogger(CommonServiceApplication.class);
+
+	@Autowired
+	private DefaultMQProducer producer;
+
+	/**
+	 * sendSingleMsg
+	 * 
+	 * @author LiuBaoWen
+	 * @return
+	 */
+	@ShellMethod(value = "DefaultMQProducer sendMsg.", key = "sendMsg")
+	public void sendMsg() throws Exception {
+		Message msg = new Message("TopicTest" /* Topic */, "TagA" /* Tag */,
+				("Hello RocketMQ " + 1).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+		);
+		// Call send message to deliver message to one of brokers.
+		SendResult sendResult = producer.send(msg);
+		log.info("{}", sendResult);
+	}
+
+	/**
+	 * syncSendMsg
+	 * 
+	 * @author LiuBaoWen
+	 * @return
+	 */
+	@ShellMethod(value = "DefaultMQProducer syncSendMsg.", key = "syncSendMsg")
+	public void syncSendMsg() throws Exception {
+		for (int i = 0; i < 10; i++) {
+			// Create a message instance, specifying topic, tag and message body.
+			Message msg = new Message("TopicTest" /* Topic */, "TagA" /* Tag */,
+					("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+			);
+			// Call send message to deliver message to one of brokers.
+			SendResult sendResult = producer.send(msg);
+			log.info("{}", sendResult);
+		}
+	}
+
+	/**
+	 * asyncSendMsg
+	 * 
+	 * @author LiuBaoWen
+	 * @return
+	 */
+	@ShellMethod(value = "DefaultMQProducer asyncSendMsg.", key = "asyncSendMsg")
+	public void asyncSendMsg() throws Exception {
+		for (int i = 0; i < 10; i++) {
+			final int index = i;
+			// Create a message instance, specifying topic, tag and message body.
+			Message msg = new Message("TopicTest", "TagA", "OrderID188",
+					"Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+			producer.send(msg, new SendCallback() {
+				@Override
+				public void onSuccess(SendResult sendResult) {
+					log.info("{}  OK {}", index, sendResult.getMsgId());
+				}
+
+				@Override
+				public void onException(Throwable e) {
+					log.info("{}  Exception {}", index, e);
+					log.error("msg:{}, err:{}", e.getMessage(), e.getStackTrace());
+				}
+			});
+		}
+	}
+
+	/**
+	 * onewaySendMsg
+	 * 
+	 * @author LiuBaoWen
+	 * @return
+	 */
+	@ShellMethod(value = "DefaultMQProducer onewaySendMsg.", key = "onewaySendMsg")
+	public void onewaySendMsg() throws Exception {
+		for (int i = 0; i < 10; i++) {
+			// Create a message instance, specifying topic, tag and message body.
+			Message msg = new Message("TopicTest" /* Topic */, "TagA" /* Tag */,
+					("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+			);
+			// Call send message to deliver message to one of brokers.
+			producer.sendOneway(msg);
+		}
+	}
+
+}
