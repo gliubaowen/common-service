4acc0beeffd2770ab3d5333779672ebee7690524
BaoWenLiu <BaoWenLiu@9.200.53.29>
2019-10-31 23:21:53 +0800

    修改 代码  删除无用的配置文件

---
+++ src/main/java/com/ibm/common/service/config/CommonServiceConfig.java
@@ -0,0 +1,22 @@
+///**
+// * 
+// */
+//package com.ibm.common.service.config;
+//
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//
+///**
+// * 应用配置
+// * 
+// * @author LiuBaoWen
+// *
+// */
+//@EnableAutoConfiguration
+//public class CommonServiceConfig {
+//
+////	@Bean
+////	public SftpService SftpService() {
+////		return new SftpService();
+////	}
+//
+//}
