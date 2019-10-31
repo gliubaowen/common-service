4af039062ed0c39c72d09fdf78bdef38b78dd055
BaoWenLiu <BaoWenLiu@9.200.53.29>
2019-11-01 00:54:37 +0800

    单元测试 引入断言

--- src/test/java/com/ibm/common/service/redis/StringRedisTemplateTest.java
+++ src/test/java/com/ibm/common/service/redis/StringRedisTemplateTest.java
@@ -3,8 +3,10 @@
  */
 package com.ibm.common.service.redis;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -16,15 +18,26 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@FixMethodOrder(MethodSorters.JVM)
 public class StringRedisTemplateTest {
 
 	@Autowired
 	private StringRedisTemplate redisTemplate;
 
 	@Test
-	public void testDemo() {
-		String string = redisTemplate.opsForValue().get("mykey");
-		System.out.println(string);
+	public void testSet() {
+		redisTemplate.opsForValue().set("key", "value");
+	}
+
+	@Test
+	public void testGet() {
+		String value = redisTemplate.opsForValue().get("key");
+		assert "value".equals(value);
+	}
+
+	@Test
+	public void testDelete() {
+		assert redisTemplate.delete("key");
 	}
 
 }
