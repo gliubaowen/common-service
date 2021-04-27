eade09b919cb2b1549f558a8549daf300164fbc1
LiuBaoWen <LiuBaoWen@9.197.230.95>
2021-04-27 16:59:31 +0800

    升级jdk 从1.8 到 11 && 删除 webservices 代码支持

--- src/main/java/com/lbw/common/service/CommonServiceApplication.java
+++ src/main/java/com/lbw/common/service/CommonServiceApplication.java
@@ -1,14 +1,10 @@
 package com.lbw.common.service;
 
-import javax.xml.ws.Endpoint;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.lbw.common.service.webservices.impl.WeatherInterfaceImpl;
 
 /**
  * 启动类
@@ -25,9 +21,6 @@ public class CommonServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommonServiceApplication.class, args);
-		
-		Endpoint.publish("http://127.0.0.1:8081/weather", new WeatherInterfaceImpl());
-		
 	}
 
 }
